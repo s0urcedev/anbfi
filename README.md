f1b8f449f02ac1b561226c00107ee7cd9379066a
s0urcedev <54136752+s0urcedev@users.noreply.github.com>
2022-07-20 21:58:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# anbfi
+
+ANBFI(Again New Brain F*ck Interpreter)
+
+Use anbfi.exe or build anbfi.cpp on your system
+
+Run: `anbfi {program name}`
+
+`HelloWorld.bf` — example of Hello World! on Brain F*ck
---
+++ anbfi.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+unsigned char arr[30000];
+short i = 0;
+
+void check_code(string code){
+    for(int j = 0; j < code.length(); j ++){
+        switch (code[j])
+        {
+        case '+':
+            arr[i] ++;
+            break;
+        case '-':
+            arr[i] --;
+            break;
+        case '>':
+            i ++;
+            break;
+        case '<':
+            i --;
+            break;
+        case '.':
+            cout << arr[i];
+            break;
+        case ',':
+            cin >> arr[i];
+            break;
+        case '[':
+            while(arr[i] != 0){
+                check_code(code.substr(j + 1, code.substr(j + 1, code.length()).find(']')));
+            }
+            j += code.substr(j + 1, code.length()).find(']') + 1;
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    ifstream indata;
+    indata.open(argv[1]);
+    string code;
+    getline(indata, code, (char)0);
+    check_code(code);
+}
